0d4d1da9a6648b7a686e6fa9a9e6070d1944742e
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-03-25 13:49:45 +0100

    addet navbar algo for changing viewport

--- template/navigation/navigation.js
+++ template/navigation/navigation.js
@@ -1,5 +1,8 @@
+/* ===================================== Navbar open close ========================================= */
+
 let navbar = true;
 
+/* === function to hide and show navbar === */
 function openCloseMenu() {
     if (navbar) {
         document.getElementById('navbar-container').classList.add('d-none');
@@ -9,3 +12,67 @@ function openCloseMenu() {
         navbar = true;
     }
 }
+
+/* ===================================== Navbar open close end ===================================== */
+
+
+/* ======================================= Navigate Join ======================================= */
+let board = 'board';
+let back_log = 'back-log';
+let add_task = 'add-task';
+let help = 'help';
+
+/* 
+template "DIV" at index.html needs a id:
+    - template board = id="board"
+    - template backLog = id="back-log"
+    - template addTask = id="add-task"
+    - template help = id="help"
+*/
+
+function openSites(site) {/* site becomes value of onclick  */
+
+    if (site == board) {
+        openBoard();
+    }
+    if (site == back_log) {
+        openBackLog();
+    }
+    if (site == add_task) {
+        openAddTask();
+    }
+    if (site == help) {
+        openHelp();
+    }
+
+}
+
+function openBoard() {
+    document.getElementById('board').classList.remove('d-none');
+    document.getElementById('back-log').classList.add('d-none');
+    document.getElementById('add-task').classList.add('d-none');
+    document.getElementById('help').classList.add('d-none');
+}
+
+function openBackLog() {
+    document.getElementById('back-log').classList.remove('d-none');
+    document.getElementById('board').classList.add('d-none');
+    document.getElementById('add-task').classList.add('d-none');
+    document.getElementById('help').classList.add('d-none');
+}
+
+function openAddTask() {
+    document.getElementById('add-task').classList.remove('d-none');
+    document.getElementById('back-log').classList.add('d-none');
+    document.getElementById('board').classList.add('d-none');
+    document.getElementById('help').classList.add('d-none');
+}
+
+function openHelp() {
+    document.getElementById('help').classList.remove('d-none');
+    document.getElementById('back-log').classList.add('d-none');
+    document.getElementById('board').classList.add('d-none');
+    document.getElementById('add-task').classList.add('d-none');
+}
+
+/* ===================================== Navigate Join end ===================================== */
