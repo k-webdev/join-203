ef52176110977fec5e6de8495c32fe708b012802
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-03-27 17:31:21 +0200

    merged 3 versions to 1. removed bugs and sort templates

--- template/navigation/navigation.js
+++ template/navigation/navigation.js
@@ -17,62 +17,33 @@ function openCloseMenu() {
 
 
 /* ======================================= Navigate Join ======================================= */
-let board = 'board';
-let back_log = 'back-log';
-let add_task = 'add-task';
-let help = 'help';
 
 /* 
 template "DIV" at index.html needs a id:
     - template board = id="board"
     - template backLog = id="back-log"
-    - template addTask = id="add-task"
+    - template addTask = id="addTask"
     - template help = id="help"
 */
 
-function openSites(site) {/* site becomes value of onclick  */
 
-    if (site == board) {
-        openBoard();
-    }
-    if (site == back_log) {
-        openBackLog();
-    }
-    if (site == add_task) {
-        openAddTask();
-    }
-    if (site == help) {
-        openHelp();
-    }
-
-}
 
 function openBoard() {
     document.getElementById('board').classList.remove('d-none');
-    document.getElementById('back-log').classList.add('d-none');
-    document.getElementById('add-task').classList.add('d-none');
-    document.getElementById('help').classList.add('d-none');
+    /* document.getElementById('back-log').classList.add('d-none'); */
+    document.getElementById('addTask').classList.add('d-none');
 }
 
-function openBackLog() {
+/* function openBackLog() {
     document.getElementById('back-log').classList.remove('d-none');
     document.getElementById('board').classList.add('d-none');
     document.getElementById('add-task').classList.add('d-none');
-    document.getElementById('help').classList.add('d-none');
+} */
+
+function openAddTasks(){
+    document.getElementById('addTask').classList.remove('d-none');
+    document.getElementById('board').classList.add('d-none');
 }
 
-function openAddTask() {
-    document.getElementById('add-task').classList.remove('d-none');
-    document.getElementById('back-log').classList.add('d-none');
-    document.getElementById('board').classList.add('d-none');
-    document.getElementById('help').classList.add('d-none');
-}
-
-function openHelp() {
-    document.getElementById('help').classList.remove('d-none');
-    document.getElementById('back-log').classList.add('d-none');
-    document.getElementById('board').classList.add('d-none');
-    document.getElementById('add-task').classList.add('d-none');
-}
 
 /* ===================================== Navigate Join end ===================================== */
