56cf16a71c9c05d515564ecaa3d0c665ef727b99
Stewz00 <markus.stitka94@gmail.com>
2022-03-30 07:49:24 +0200

    done except assign and data form

--- template/addItem/addItem.js
+++ template/addItem/addItem.js
@@ -1,30 +1,50 @@
-/* let title;
+let title;
 let dueDate;
-let category;
+let department;
 let urgency;
 let description;
 let assignedEmployees = [];
 
-function onInit(){
-
+function addTaskOnInit() {
+    setTodayInDatepicker();
 }
 
 function getInputValues() {
     title = document.getElementById('title').value
     dueDate = document.getElementById('date').value
-    category = document.getElementById('category').value
+    department = document.getElementById('department').value
     urgency = document.getElementById('urgency').value
     description = document.getElementById('description').value
 
-    console.log(title, dueDate, category, urgency, description)
+    console.log(title, dueDate, department, urgency, description)
     // let employeesAssigned = document.getElementById('employeesAssi').value
 }
 function assignEmployee() {
-   
+
 }
 function createTask() {
 
+    resetInputFields();
 }
 function cancelTask() {
 
-} */
+}
+
+function getToday() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+    var yyyy = today.getFullYear();
+    return yyyy + "-" + mm + "-" + dd
+}
+function setTodayInDatepicker() {
+    dueDate = document.getElementById('date').value = getToday();
+}
+
+function resetInputFields() {
+    title = document.getElementById('title').value = "";
+    dueDate = document.getElementById('date').value = getToday();
+    department = document.getElementById('department').value = "Management"
+    urgency = document.getElementById('urgency').value = "High"
+    description = document.getElementById('description').value = "";
+}
