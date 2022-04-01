1efb6e97512ffa0a1a8b848553cf40575e09c8ff
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-04-01 11:47:49 +0200

    completed backlog design

--- template/backlog/backlog.js
+++ template/backlog/backlog.js
@@ -5,84 +5,117 @@ let tasksForBacklog = [
         mail: 'Dennis.Kemnade@live.de',
         image: './../../img/homer.jpg',
         cathegory: 'Developer',
-        details: 
-        `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
          Quia ipsam, culpa laudantium soluta molestiae qui,
          saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
-         index: '0'
+        index: '0'
     },
     {
         name: 'Dennis Kemnade',
         mail: 'Dennis.Kemnade@live.de',
         image: './../../img/homer.jpg',
         cathegory: 'Developer',
-        details: 
-        `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
          Quia ipsam, culpa laudantium soluta molestiae qui,
          saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
-         index: '0'
+        index: '0'
     },
     {
         name: 'Dennis Kemnade',
         mail: 'Dennis.Kemnade@live.de',
         image: './../../img/homer.jpg',
         cathegory: 'Developer',
-        details: 
-        `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
          Quia ipsam, culpa laudantium soluta molestiae qui,
          saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
-         index: '0'
+        index: '0'
     },
     {
         name: 'Dennis Kemnade',
         mail: 'Dennis.Kemnade@live.de',
         image: './../../img/homer.jpg',
         cathegory: 'Developer',
-        details: 
-        `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
          Quia ipsam, culpa laudantium soluta molestiae qui,
          saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
-         index: '0'
+        index: '0'
     },
     {
         name: 'Dennis Kemnade',
         mail: 'Dennis.Kemnade@live.de',
         image: './../../img/homer.jpg',
         cathegory: 'Developer',
-        details: 
-        `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
          Quia ipsam, culpa laudantium soluta molestiae qui,
          saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
-         index: '0'
+        index: '0'
     },
     {
         name: 'Dennis Kemnade',
         mail: 'Dennis.Kemnade@live.de',
         image: './../../img/homer.jpg',
         cathegory: 'Developer',
-        details: 
-        `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
          Quia ipsam, culpa laudantium soluta molestiae qui,
          saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
-         index: '0'
+        index: '0'
     },
     {
         name: 'Dennis Kemnade',
         mail: 'Dennis.Kemnade@live.de',
         image: './../../img/homer.jpg',
         cathegory: 'Developer',
-        details: 
-        `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
          Quia ipsam, culpa laudantium soluta molestiae qui,
          saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
-         index: '0'
+        index: '0'
+    },
+    {
+        name: 'Dennis Kemnade',
+        mail: 'Dennis.Kemnade@live.de',
+        image: './../../img/homer.jpg',
+        cathegory: 'Developer',
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+         Quia ipsam, culpa laudantium soluta molestiae qui,
+         saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
+        index: '0'
+    },
+    {
+        name: 'Dennis Kemnade',
+        mail: 'Dennis.Kemnade@live.de',
+        image: './../../img/homer.jpg',
+        cathegory: 'Developer',
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+         Quia ipsam, culpa laudantium soluta molestiae qui,
+         saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
+        index: '0'
+    },
+    {
+        name: 'Dennis Kemnade',
+        mail: 'Dennis.Kemnade@live.de',
+        image: './../../img/homer.jpg',
+        cathegory: 'Developer',
+        details:
+            `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus iure itaque vitae! 
+         Quia ipsam, culpa laudantium soluta molestiae qui,
+         saepe possimus expedita itaque vel in a reprehenderit nostrum vero. Reiciendis.`,
+        index: '0'
     }
 ];
 
 
 
-function initBacklog(){/* inition by onclick="openBacklog(addBacklog())" at navigation.html */
-    addBacklog();
+function initBacklog() {/* inition by onclick="openBacklog(addBacklog())" at navigation.html */
+    addBacklogs();
 }
 
 /**
@@ -91,40 +124,34 @@ function initBacklog(){/* inition by onclick="openBacklog(addBacklog())" at navi
  * @param{ string } - JSON array 
  */
 
-function addBacklog(){
-    document.getElementById('add-backlogs').innerHTML = '';
-
-    document.getElementById('add-backlogs').innerHTML += `
-    
-        <div class="backlog-headlines">
-            <h5>ASSIGNED TO</h5><h5>CATEGORY</h5><h5>DETAILS</h5>
-        </div>
-
-    `;
+function addBacklogs() {
+    document.getElementById('add-table-content').innerHTML = '';
 
     for (let i = 0; i < tasksForBacklog.length; i++) {
-        
-    
-        document.getElementById('add-backlogs').innerHTML += `
-        
-        <div class="backlogs-design">
-            <div class="backlog-image">
-                <img src="${tasksForBacklog[i]['image']}">
-            </div>
-            <div class="assigned-to">
-                <h4>${tasksForBacklog[i]['name']}</h4>
-                <a href="mailto:${tasksForBacklog[i]['mail']}">${tasksForBacklog[i]['mail']}</a>
-            </div>
-            <div class="backlog-cathegory">
-                <p>${tasksForBacklog[i]['cathegory']}</p>
-            </div>
-            <div class="backlog-details">
-                <span>
-                    ${tasksForBacklog[i]['details']}
-                </span>
-            </div>
-        </div>
 
+        document.getElementById('add-table-content').innerHTML += `
+        
+                <tr class="table-body-data-sort">
+                    <td>
+                        <img class="img-assigned" src="${tasksForBacklog[i]['image']}">
+                    </td>
+                    <td>
+                        <div>
+                            <h4>${tasksForBacklog[i]['name']}</h4>
+                            <a class="link-design" href="mailto:${tasksForBacklog[i]['mail']}">${tasksForBacklog[i]['mail']}</a>
+                        </div>
+                    </td>
+                    <td>
+                        <p>${tasksForBacklog[i]['cathegory']}</p> 
+                    </td>
+                    <td>
+                        ${tasksForBacklog[i]['details']}
+                    </td>
+                </tr>  
+        
         `;
+
     }
+
+
 }
