893ff209e0fe54a2b2113d8d693397f93ee79349
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-04-04 20:13:30 +0200

    merge from main

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-/* Variables and Arrays */
-
-let addTasksAsJSON = [];// add tasks as array, local for single session
-let addTasksToJSonAsString = [];// add tasks stringifyed, for localstorage
-let tasksForBacklog = [];
-let taskAsString = [];
-let taskFromLocalStorage = [];
-
-function loadArrayFromLocalStorage() {
-
-    taskFromLocalStorage = localStorage.getItem('tasks');
-
-    console.log(taskFromLocalStorage);
-
-    tasksForBacklog.push(JSON.parse(JSON.stringify(taskFromLocalStorage)));
-
-    console.log(tasksForBacklog);
-    console.log(addTasksToJSonAsString);
-    console.log(addTasksAsJSON);
-
-}
-
-
-/* ============================= Add tasks to backlog ============================= */
-
-
-function initBacklog() {/* inition by onclick="openBacklog(addBacklog())" at navigation.html */
-    loadArrayFromLocalStorage();
-    addBacklogs();
-}
-
 /**
  * Using the tasksForBacklog JSON to fill backlog content.
  * 
@@ -38,10 +7,7 @@ function initBacklog() {/* inition by onclick="openBacklog(addBacklog())" at nav
 function addBacklogs() { //img tag variable for changing images. ${tasksForBacklog[x][i]['image']}
     document.getElementById('add-table-content').innerHTML = '';
 
-    
-
-
-        for (let i = 0; i < tasksForBacklog.length; i++) {
+    for (let i = 0; i < tasks.length; i++) {
 
             document.getElementById('add-table-content').innerHTML += `
         
@@ -51,74 +17,17 @@ function addBacklogs() { //img tag variable for changing images. ${tasksForBackl
                     </td>
                     <td>
                         <div>
-                            <h4>${tasksForBacklog[i]['title']}</h4>
-                            <a class="link-design" href="mailto:${tasksForBacklog[i]['mail']}">${tasksForBacklog[i]['mail']}</a>
+                            <h4>${tasks[i]['title']}</h4>
                         </div>
                     </td>
                     <td>
-                        <p>${tasksForBacklog[i]['category']}</p> 
+                        <p>${tasks[i]['department']}</p> 
                     </td>
                     <td>
-                        ${tasksForBacklog[i]['description']}
+                        ${tasks[i]['description']}
                     </td>
                 </tr>  
         
             `;
         }
-    
 }
-/* ============================= Add tasks to backlog end ============================= */
-
-/* ================================== create Array for Backlog ================================== */
-
-
-/**
- * Elements get stringifyed and saved in localstorage.
- * 
- * 
- * @param {} - 
- */
-function createTask() {
-
-    let title = document.getElementById('title').value; //Title from add Task
-    let category = document.getElementById('category').value;
-    let urgency = document.getElementById('urgency').value;
-    let date = document.getElementById('date').value; //Date from Datepicker on add Task
-    let description = document.getElementById('description').value; // description content from add task
-
-    let task =
-        [
-            {
-                'title': title,
-                'category': category,
-                'urgency': urgency,
-                'date': date,
-                'description': description
-            }
-        ];
-
-    addTasksAsJSON.push(task);
-
-    for (let i = 0; i < addTasksAsJSON.length; i++) {
-        const task = addTasksAsJSON[i];
-
-        /* console.log(task); */
-
-        taskAsString.push(JSON.stringify(task)); //addTasksToJSonAsString is a JSON as String
-
-        console.log(taskAsString);
-
-        localStorage.setItem('tasks', taskAsString); //add tasks to local storage
-    }
-    removeInputs();
-}
-
-function removeInputs() {
-    document.getElementById('title').value = ''; //Title from add Task
-    document.getElementById('category').value = '';
-    document.getElementById('urgency').value = '';
-    document.getElementById('date').value = ''; //Date from Datepicker on add Task
-    document.getElementById('description').value = ''; // description content from add task
-}
-
-/* ================================== create Array for Backlog end ================================== */
