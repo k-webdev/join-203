b0520424bac1511f01fb2130007cba67df43e269
Stewz00 <markus.stitka94@gmail.com>
2022-04-06 09:21:13 +0200

    relocating assignTo dropdown buggy

--- template/addItem/addItem.js
+++ template/addItem/addItem.js
@@ -17,6 +17,7 @@ let allEmployeePicture = [{
 
 function addTaskOnInit() {
     setTodayInDatepicker();
+    assignButtonInit();
 }
 function createTask() {
     getInputValues();
@@ -24,7 +25,6 @@ function createTask() {
     pushTaskIntoTasks();
     saveDB();
     resetInputFields();
-
 }
 
 function getInputValues() {
@@ -53,14 +53,36 @@ function getTask() {
     return task;
 }
 
-function assignButton() {
-    let assignableEmployees = allEmployees;
+function assignButtonInit() {
+    // let assignedEmployees = allEmployees;
 
-    for (let i = 0; i < assignableEmployees.length; i++) {
+    document.getElementById('assign-btn-container').innerHTML =
+        `<select id="assign-options"
+        class="btn btn-outline-secondary dropdown-toggle my-2 dropdown-style selection-colors" type="button"
+        aria-expanded="false" placeholder="Select">
+    </select>`;
+
+    for (let i = 0; i < allEmployees.length; i++) {
         document.getElementById('assign-options').innerHTML +=
             `
-        <option onclick="sliceAssignedEmployee(${i, assignableEmployees})" class="selection-colors" value="${assignableEmployees[i]}">${assignableEmployees[i]}</option>
+        <option onclick="spliceAssignedEmployee(${i, allEmployees[i]})" class="selection-colors" value="${allEmployees[i]}">${allEmployees[i]}</option>
         `
+    }
+}
+function repositionDropdown(){
+
+}
+// need to fix function
+function assignEmployee() {
+    // document.getElementById('button').addEventListener("click", onmousedown);
+    onmousedown = function (e) {
+        let x = e.x;
+        let y = e.y;
+        if (document.getElementById('assign-btn').clicked == true) {
+            document.getElementById('assign-options').style.left = `${e.x / window.innerWidth * 100}%`
+            document.getElementById('assign-options').style.top = `${e.y / window.innerHeight * 100}%`
+        }
+        console.log(x, y)
     }
 }
 
@@ -72,10 +94,7 @@ function spliceAssignedEmployee(i, assignableEmployees) {
     return assignableEmployees.splice(i);
 }
 
-function assignEmployee() {
-    //<img class="img-item" src="./../../img/homer.jpg">
-    //splice assignedEmployees from chooseable employees
-}
+
 
 
 function cancelTask() {
@@ -98,6 +117,6 @@ function resetInputFields() {
     title = document.getElementById('title').value = "";
     dueDate = document.getElementById('date').value = getToday();
     department = document.getElementById('department').value = "Management"
-    urgency = document.getElementById('urgency').value = "High"
+    urgency = document.getElementById('urgency').value = "high"
     description = document.getElementById('description').value = "";
 }
