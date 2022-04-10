ba81f44ba66a83cbe8c7fb85530d84596b5920de
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-04-10 12:15:04 +0200

    change Backlog elements

--- template/help/help.js
+++ template/help/help.js
@@ -21,16 +21,8 @@ function openBackLogHelp() {
     
     <span>
 
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur perferendis, ullam deserunt suscipit exercitationem nam itaque ipsum cupiditate? Necessitatibus, recusandae! Sunt dolor animi non accusamus placeat voluptatum magni autem pariatur!
-Maxime beatae doloremque dolore natus aliquid voluptatem incidunt, praesentium fugiat consequatur culpa eaque mollitia repudiandae minus voluptatibus placeat, rerum deleniti nostrum! Necessitatibus architecto officiis iure exercitationem. Consequuntur voluptates nulla id!
-Similique vitae voluptatibus pariatur, voluptatem quidem tempore sunt facilis fugit necessitatibus sint, dicta, deleniti dolore minus recusandae amet? Provident velit explicabo animi praesentium quas eum. Dolore iure quidem sunt aspernatur.
-Assumenda maxime eligendi nesciunt velit suscipit et id nulla. Est perspiciatis neque nihil necessitatibus? Eaque iste, ducimus, numquam minima nemo placeat eveniet officiis deleniti dolorem porro, enim ratione cupiditate recusandae!
-Error facere, necessitatibus sunt voluptas architecto assumenda. Aut sit laborum aliquam, ipsum laudantium ratione voluptate esse nostrum consequuntur numquam illum qui ipsa. Quo eaque harum eos, tempora dicta a quisquam!
-Odit animi repudiandae nihil atque. Tempora maiores illum voluptatem nemo distinctio et eum, commodi est quis rerum fuga iure! Tenetur nulla ipsa nemo fugit illo distinctio ut, commodi recusandae deleniti.
-Doloremque aperiam ea tempore explicabo repellendus atque quasi corporis vero. Cumque minima id consequuntur architecto temporibus quae odit error laborum ea nobis minus expedita rerum voluptate quos possimus, debitis in.
-Voluptatibus nam necessitatibus animi tempore cum voluptate illum earum, ipsum quis adipisci delectus fuga mollitia magnam ea quibusdam fugit natus repellendus accusantium facere excepturi ipsa? Consequatur doloribus officiis a ipsum!
-Delectus tenetur corporis iste blanditiis maiores natus molestias non eveniet. Explicabo, consequatur. Impedit, modi iusto minima voluptatum deserunt facilis itaque repudiandae tempora sit quaerat dolorem provident dolorum minus illo recusandae!
-Error iste nisi quos officia vero voluptas voluptatem commodi facere! Officiis velit alias sit aspernatur dolorum tempora aliquam, repellat natus rerum enim eius facere mollitia illo, incidunt placeat. Aliquid, repellat?
+    <p><b>Backlog:</b></p>
+    Here you will find an overview of all the tasks.
 
     </span>
 
@@ -44,16 +36,20 @@ function openAddTask() {
     
     <span>
 
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur perferendis, ullam deserunt suscipit exercitationem nam itaque ipsum cupiditate? Necessitatibus, recusandae! Sunt dolor animi non accusamus placeat voluptatum magni autem pariatur!
-Maxime beatae doloremque dolore natus aliquid voluptatem incidunt, praesentium fugiat consequatur culpa eaque mollitia repudiandae minus voluptatibus placeat, rerum deleniti nostrum! Necessitatibus architecto officiis iure exercitationem. Consequuntur voluptates nulla id!
-Similique vitae voluptatibus pariatur, voluptatem quidem tempore sunt facilis fugit necessitatibus sint, dicta, deleniti dolore minus recusandae amet? Provident velit explicabo animi praesentium quas eum. Dolore iure quidem sunt aspernatur.
-Assumenda maxime eligendi nesciunt velit suscipit et id nulla. Est perspiciatis neque nihil necessitatibus? Eaque iste, ducimus, numquam minima nemo placeat eveniet officiis deleniti dolorem porro, enim ratione cupiditate recusandae!
-Error facere, necessitatibus sunt voluptas architecto assumenda. Aut sit laborum aliquam, ipsum laudantium ratione voluptate esse nostrum consequuntur numquam illum qui ipsa. Quo eaque harum eos, tempora dicta a quisquam!
-Odit animi repudiandae nihil atque. Tempora maiores illum voluptatem nemo distinctio et eum, commodi est quis rerum fuga iure! Tenetur nulla ipsa nemo fugit illo distinctio ut, commodi recusandae deleniti.
-Doloremque aperiam ea tempore explicabo repellendus atque quasi corporis vero. Cumque minima id consequuntur architecto temporibus quae odit error laborum ea nobis minus expedita rerum voluptate quos possimus, debitis in.
-Voluptatibus nam necessitatibus animi tempore cum voluptate illum earum, ipsum quis adipisci delectus fuga mollitia magnam ea quibusdam fugit natus repellendus accusantium facere excepturi ipsa? Consequatur doloribus officiis a ipsum!
-Delectus tenetur corporis iste blanditiis maiores natus molestias non eveniet. Explicabo, consequatur. Impedit, modi iusto minima voluptatum deserunt facilis itaque repudiandae tempora sit quaerat dolorem provident dolorum minus illo recusandae!
-Error iste nisi quos officia vero voluptas voluptatem commodi facere! Officiis velit alias sit aspernatur dolorum tempora aliquam, repellat natus rerum enim eius facere mollitia illo, incidunt placeat. Aliquid, repellat?
+    <b>Title:</b> Title that is as meaningful as possible.<br><br>
+    
+    <b>Department/Urgency:</b> Drop-down to select the relevant area.<br><br>
+
+    <b>Date:</b> Set the start date for the task.<br><br>
+    
+    <b>Description:</b> Description of the activity as detailed as possible.<br><br>
+    
+    <b>Assigned To:</b> Clicking on + leads to a dropdown, which after selection adds the picture of the selected employee. 
+    To delete the employee, click on the employee's picture. <br><br>
+    
+    <b>Create Task:</b> Passes the entered data to the backlog, where the data is presented in an overview. <br><br>
+    
+    <b>Cancel:</b> Leads to the closing of the add-task dialog window and deletes all entries that have been made so far.
 
     </span>
 
