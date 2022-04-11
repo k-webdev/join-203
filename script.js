477ff47cbb6b6374cb6d8cc2b3231e5e48104053
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-04-11 11:54:06 +0200

    addet backlog functions

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+
+
+
+
 /**
+ * function addBacklogs()
+ * 
  * Using the tasksForBacklog JSON to fill backlog content.
  * 
  * @param{ string } - JSON array 
@@ -32,15 +38,56 @@ function addBacklogs() { //img tag variable for changing images. ${tasksForBackl
         }
 }
 
-function openActionForBacklogElements(){
-    document.getElementById('back-log').innerHTML += `
+function openActionForBacklogElements(index){
+    document.getElementById('details-content').classList.remove('d-none');
+    document.getElementById('title-details').innerHTML = ` ${tasks[index]['title']}`;
+    document.getElementById('assigned-to-details').innerHTML = ` ${tasks[index]['assigned-to']}`;
+    document.getElementById('department-details').innerHTML = ` ${tasks[index]['department']}`;
+    document.getElementById('description-details').innerHTML = ` ${tasks[index]['description']}`;
+    document.getElementById('change-department').innerHTML = `
     
-    <div class="action-for-backlog">
-        <div class="action-for-backlog-content">
-        test
-        </div>
-    </div>
+    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                <input onclick="changeDepartment('management', ${index})" type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
+                <label class="btn btn-outline-success" for="btnradio1">Managment</label>
+              
+                <input onclick="changeDepartment('finance', ${index}))" type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                <label class="btn btn-outline-success" for="btnradio2">Finance</label>
+              
+                <input onclick="changeDepartment('it', ${index}))" type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                <label class="btn btn-outline-success" for="btnradio3">IT</label>
+                
+                <input onclick="changeDepartment('controlling', ${index}))" type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
+                <label class="btn btn-outline-success" for="btnradio4">Controlling</label>
 
+                <input onclick="changeDepartment('web', ${index}))" type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off">
+                <label class="btn btn-outline-success" for="btnradio5">Web</label>
+              </div>
+    
     `;
+    document.getElementById('add-to-backlog-btn').innerHTML = `
+
+    
+  
+
+  <button onclick="addToBacklog(${index}); closeDetails()" type="button" class="btn btn-success">Show on Board</button>
+  <button onclick="closeDetails()" type="button" class="btn btn-danger">Close</button>
+  
+    
+    `;
+    
 }
 
+function closeDetails(){
+    document.getElementById('details-content').classList.add('d-none');
+    document.getElementById('assigned-to-details').innerHTML = '';   
+}
+
+function changeDepartment(department, index){
+    tasks[index]['department'] = department;
+}
+
+
+function addToBacklog(index){
+    tasks[index]['visibleboard'] = true;
+    saveDB();
+}
