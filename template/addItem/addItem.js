9815665e170cbeb7dff9d237e288c7391373820c
Stewz00 <markus.stitka94@gmail.com>
2022-04-11 10:56:39 +0200

    jsdoc documentation

--- template/addItem/addItem.js
+++ template/addItem/addItem.js
@@ -1,3 +1,5 @@
+
+
 let tasks = [];
 let title;
 let dueDate;
@@ -15,23 +17,39 @@ let assignableEmployees;
 let assignedEmployees = [];
 
 //mouseclick eventlistener
+
 onmousedown = function (e) {
     mousePosition_x = e.x;
     mousePosition_y = e.y;
 }
-
+/**
+ * This function sets @var {array} assignableEmployees - to default value of all assignableEmployees,
+ * calls @function setTodayInDatepicker() and
+ * calls @function setTodayInDatepicker() and
+ * calls @function onmousedown to setup first opening of addTask
+ * 
+ */
 function addTaskOnInit() {
     assignableEmployees=['Homer', 'Marge', 'Maggie', 'Bart', 'Lisa'];
     setTodayInDatepicker();
     updateAssignableEmployeeList();
     onmousedown;
 }
-// today function
+/**
+ * Function sets @var {string} dueDate to todays date by assigning
+ * @function getToday() to 
+ * @var {string} dueDate
+ */
 function setTodayInDatepicker() {
     dueDate = document.getElementById('date').value = getToday();
 }
 
-//cancel or finished creating task functions
+/**
+ * This function gets input values, check if there are empty fields,
+ * push values into an JSON Array and then get it into the backend 
+ * after that it cleans up and sets everything to default
+ * @returns 
+ */
 function createTask() {
     getInputValues();
     //if title or description is empty function stops
@@ -45,6 +63,9 @@ function createTask() {
     alert('Task created!');
 }
 
+/**
+ * This sets all input fields to default
+ */
 function resetInputFields() {
     title = document.getElementById('title').value = "";
     dueDate = document.getElementById('date').value = getToday();
@@ -54,14 +75,19 @@ function resetInputFields() {
     assignedEmployees = [];
     assignableEmployees = ['Homer', 'Marge', 'Maggie', 'Bart', 'Lisa'];
 }
-
+/**
+ * This closes @template AddTaks 
+ */
 function closeAddTasks() {
     resetInputFields();
     document.getElementById('addTask').classList.add('d-none');
     document.getElementById('back-log').classList.add('d-none');
     document.getElementById('board').classList.add('d-none');
 }
-//update functions
+//update functinos
+/**
+ * This updates and render dropdown menu of assignable employee for that task
+ */
 function updateAssignableEmployeeList() {
 
     document.getElementById('assign-btn-container').innerHTML =
@@ -79,12 +105,21 @@ function updateAssignableEmployeeList() {
     }
     document.getElementById('assign-options').innerHTML += `<option  value="close" class="selection-colors">Close dropdown</option>`;
 }
-
+/**
+ * reposition dropdown menu at position 
+ * @var {number} mousePosition_x and 
+ * @var {number} mousePosition_x 
+ */
 function repositionAssignToDropdown() {
     document.getElementById('assign-options').style.left = `${mousePosition_x / window.innerWidth * 100}%`
     document.getElementById('assign-options').style.top = `${mousePosition_y / window.innerHeight * 100}%`
 }
-
+/**
+ * This adds assigned employee to 
+ * @var {array} assignedEmployees and removes assigned employee at position 
+ * @param {number} indexOfRemovedEmployee from 
+ * @var {array} assignableEmployees 
+ */
 function removeAssignedEmployee(indexOfRemovedEmployee){
     //update arrayLists
     assignableEmployees.push(assignedEmployees[indexOfRemovedEmployee]);
@@ -92,7 +127,9 @@ function removeAssignedEmployee(indexOfRemovedEmployee){
     renderAssignedEmployees();
     updateAssignableEmployeeList();
 }
-
+/**
+ * This renders img of assigned employees
+ */
 function renderAssignedEmployees() {
     document.getElementById('assigned-to-container').innerHTML = '';
     for (let i = 0; i < assignedEmployees.length; i++) {
@@ -101,12 +138,20 @@ function renderAssignedEmployees() {
 }
 
 //dropdown handle functions
+/**
+ * This opens dropdown menu and reposition it if 
+ * @var {array} assignableEmployees is not empty
+ */
 function openAssignEmployeeDropdown() {
     if (assignableEmployees == 0) { return }
     document.getElementById('assign-btn-container').style.display = '';
     repositionAssignToDropdown();
 }
 
+/**
+ * This selects an assignable employee from dropdown menu and calls functions to update and render
+ * @var {array} assignableEmployees 
+ */
 function selectAssignableEmployee() {
     var dropdownMenu = document.getElementById('assign-options');
     var selectedEmployee = dropdownMenu.options[dropdownMenu.selectedIndex].value;
@@ -127,6 +172,10 @@ function selectAssignableEmployee() {
 }
 
 //getter functions
+/**
+ * This function gets todays date
+ * @returns {string}
+ */
 function getToday() {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
@@ -134,7 +183,9 @@ function getToday() {
     var yyyy = today.getFullYear();
     return yyyy + "-" + mm + "-" + dd
 }
-
+/**
+ * This gets input values by document id and assigns it to its variable
+ */
 function getInputValues() {
     title = document.getElementById('title').value;
     dueDate = document.getElementById('date').value;
@@ -142,7 +193,18 @@ function getInputValues() {
     urgency = document.getElementById('urgency').value;
     description = document.getElementById('description').value;
 }
-
+/**
+ * This summarizes the necessary variables (
+ * @var {number} tasks.length
+ * @var {string} title
+ * @var {string} dueDate
+ * @var {string} department
+ * @var {string} urgency
+ * @var {string} description
+ * @var {array} assignedEmployees
+ * ) + "section" and if it should be visible on board and
+ * @returns {JSON}
+ */
 function getTask() {
     let task = {
         'id': tasks.length,
@@ -159,10 +221,18 @@ function getTask() {
 }
 
 //array manipulation functions
+/**
+ * This pushes
+ * @returns {JSON} into @var {JSONArray} tasks
+ */
 function pushTaskIntoTasks() {
     return tasks.push(getTask());
 }
-
+/**
+ * This splice assigned Employees from 
+ * @var {array} assignableEmployees at position
+ * @param {number} i 
+ */
 function spliceAssignedEmployee(i) {
     assignableEmployees.splice(i, 1);
 }
